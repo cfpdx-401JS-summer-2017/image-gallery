db1ac4073b9ab9c036c4c87a0dac1ec9bdeba9a3
Mark Petrie <petrie.mark@gmail.com>
2017-08-23 11:14:48 -0700

    Add initial components (ImageList, View) and view functions

---
+++ bunny-gallery/src/image.js
@@ -0,0 +1,7 @@
+
+export function bootstrap() {
+return [
+    { title: 'cute bunny', description: 'very cute bunny', url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg'},
+    { title: 'another cute bunny', description: 'very, very cute bunny', url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-25__605.jpg'},
+    { title: 'fairly cute bunny', description: 'pretty much a cute bunny', url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-110__605.jpg'},
+]}
