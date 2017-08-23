08bf7d896f10642cab55567b30d20895ce2f5228
Joe Churchman <churchman.joe@gmail.com>
2017-08-23 13:18:10 -0700

    wrote another app test, fails

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it('loads a view', () => {
+    const wrapper = shallow( <App /> )
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+});
