18997cea095b0f9990042eff74c7d5a840a15748
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-08-23 10:22:39 -0700

    List component started

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 ListItem.propTypes = {
@@ -12,7 +12,26 @@ export function ListItem({ title, description, url }) {
         <ul>
             <li>{title}</li>
             <li>{description}</li>
-            <li>{url}</li>
+            <li><a target="_blank" href="{url}">{url}</a></li>
         </ul>
     );
+}
+
+export class List extends Component {
+
+    static propTypes = {
+        listArray: PropTypes.arrayOf(PropTypes.string).isRequired
+    }
+
+    render() {
+        const { title, description, url, ListArray } = this.props;
+        return (
+            <div>
+                {ListArray.map((item, i) => (
+                    <ListItem key={i} title={title} description={description} url={url} />
+                ))}
+            </div>
+        );
+        
+    }
 }
