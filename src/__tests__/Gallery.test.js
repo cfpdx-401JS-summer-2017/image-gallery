e9b7276e3499bc365659761ffd2d9bbfc70a143a
Joe Churchman <churchman.joe@gmail.com>
2017-08-23 13:05:26 -0700

    passed Gallery test, added travis.yml

---
+++ src/__tests__/Gallery.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import toJSON from 'enzyme-to-json';
+import Bunnies from '../images/bunnies';
+import Gallery from '../gallery/Gallery';
+
+describe( 'Gallery', () => {
+
+    it( 'displays bunnies in a gallery', () => {
+        const wrapper = shallow(<Gallery bunnies={Bunnies}/>);
+        expect(toJSON(wrapper)).toMatchSnapshot();
+    });
+
+});
