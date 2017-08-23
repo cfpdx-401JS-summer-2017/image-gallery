69bf1ac4bf9d162b6c2d5aab90f59f7d24fe7e8e
Anwar Montasir <anwarmontasir@gmail.com>
2017-08-23 11:38:50 -0700

    switch between views

--- src/App.js
+++ src/App.js
@@ -5,24 +5,55 @@ import { List } from './Components/List';
 import { Thumbs } from './Components/Thumbs';
 import { Gallery } from './Components/Gallery';
 
+function listView({ bunnies }) {
+  return <div><h2>List View</h2> <List listArray={ bunnies } /> </div>;
+}
+function thumbView({ bunnies }) {
+  return <div><h2>Thumbnail View</h2> <Thumbs thumbArray={ bunnies } /> </div>;
+}
+function galleryView({ bunnies }) {
+  return <div><h2>Gallery View</h2> <Gallery galleryArray={ bunnies } /> </div>;
+}
+
+const viewDict = {
+  list: listView,
+  thumbs: thumbView,
+  gallery: galleryView
+}
+
+const viewArray = Object.keys(viewDict);
+
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      bunnies: bunnyBuild()
+      bunnies: bunnyBuild(),
+      view: viewArray[0],
+      views: viewArray
     };
   }
 
   render() {
-    const { bunnies } = this.state;
+    const { bunnies, views, view } = this.state;
+    const ViewWrapper = viewDict[view];
 
     return (
-      <div className="App">
-        <List listArray={bunnies} />
-        <Thumbs thumbArray={bunnies} />
-        <Gallery galleryArray={bunnies} />
-      </div>
+      <main>
+        <header>
+          <h1>Cute Bunnies App</h1>
+        </header>
+        <section>
+          {views.map(v => (
+            <button key={v} onClick={() => this.setState({ view: v })}>
+              {v}
+            </button>
+          ))}
+        </section>
+        <section>
+          <ViewWrapper bunnies={ bunnies } />
+        </section>
+      </main>
     );
   }
 }
