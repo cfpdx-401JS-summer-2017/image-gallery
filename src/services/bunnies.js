d8668bb4d609746f5f02cd5de4453afdb83ccb51
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-08-23 09:28:45 -0700

    Initial Files and bunnies

---
+++ src/services/bunnies.js
@@ -0,0 +1,19 @@
+export function bunnyBuild() {
+    return [
+        {
+            title: 'Cute Bunny 1',
+            description: 'Who\'s a cute bunny??',
+            url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg'
+        },  
+        {
+            title: 'Cute Bunny 2',
+            description: 'Diabolically cute!!',
+            url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-25__605.jpg'
+        },  
+        {
+            title: 'Cute Bunny 3',
+            description: 'There are no bounds to the cuteness!',
+            url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-110__605.jpg'
+        }
+    ];
+}
