54145aa0bb62f888da55951761d0b147fb7f6a04
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-08-23 12:18:20 -0700

    rough draft of app and list files, throwing error on StevenDetail currently

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 import React, { Component } from 'react';
+import stevens from './images/Stevens';
+import StevenList from './StevenList';
 import './App.css';
 
+function StevenDetail({steven}) {
+  return <div>Steven Detail {steven}</div>;
+}
+function StevenThumbnail({steven}) {
+  return <div>Steven Thumbnail {steven}</div>
+}
+function StevenGallery({steven}) {
+  return <div>Steven Gallery {steven}</div>;
+}
+
+const View = {
+  Detail: StevenDetail,
+  Thumbnail: StevenThumbnail,
+  Gallery: StevenGallery
+}
+
+const views = Object.keys(View);
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      steven: stevens,
+      view: views[0],
+      views: views
+    }
+  }
+
   render() {
+    const {steven, views, view} = this.state;
+    const StevenView = View[view];
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Steve Pics Dot Com</h2>
+      <main>
+        <div className="App">
+          <header className="App-header">
+            <h1>Steve Pics Dot Com</h1>
+          </header>
+          <section>
+            {views.map(v => (
+              <button key={v} onClick={() => this.setState({view: v})}>
+                {v}
+              </button>
+            ))}
+          <StevenView steven={steven}/>
+          </section>
+          <section>
+            <StevenList steven={steven}/>
+          </section>
+          <p className="App-intro">
+            Thank you for looking at my pictures of Steven.
+          </p>
         </div>
-        <p className="App-intro">
-          Thank you for looking at my pictures of Steven.
-        </p>
-      </div>
+      </main>
     );
   }
 }
---
+++ src/StevenList.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import stevens from './images/Stevens';
+
+export default class Stevens extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            stevens: []
+        };
+    }
+
+    render() {
+        const {stevens} = this.state;
+        return (
+            <ul>
+                {stevens && stevens.map(steven => (
+                    <li key={steven.title}>
+                        <Steven steven={steven}/>
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+export function Steven({stevens}) {
+    return (
+        <div style={{
+            padding: '10px',
+            clear: 'both'
+        }}>
+            <img src={stevens.url}
+                alt={stevens.title}
+                style={{
+                    float: 'left',
+                    width: 100
+                }}/>
+                <span>{stevens.title}</span>
+        </div>
+    );
+}
