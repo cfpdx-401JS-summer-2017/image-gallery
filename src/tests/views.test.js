be0196bc43ea7d5afc9f76099459061e3274466f
christylaguardia <christinelaguardia@gmail.com>
2017-08-24 12:12:28 -0700

    added snapshot tests for app and three views

--- src/tests/views.test.js
+++ src/tests/views.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../App';
+import renderer from 'react-test-renderer'
 
 import EmojiList from '../views/list';
 import EmojiThumbnail from '../views/thumbnail';
@@ -8,23 +8,66 @@ import EmojiGallery from '../views/gallery';
 
 describe('views components', () => {
 
-    it('renders the LIST view  without crashing', () => {
-        const testEmojis = [];
-        const div = document.createElement('div');
-        ReactDOM.render(<EmojiList emojis={testEmojis}/>, div);
+    const testEmojis = [
+        {
+          id: 1,
+          title: 'syringe',
+          description: 'nurses poke you with it',
+          url: 'https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/96/syringe_1f489.png'
+        },{
+          id: 2,
+          title: 'sushi',
+          description: 'super yummy!',
+          url: 'https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/96/sushi_1f363.png',
+        },{
+          id: 3,
+          title: 'pile of poo',
+          description: 'what I look like doing snapshot testing',
+          url: 'https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/96/pile-of-poo_1f4a9.png',
+        }
+    ];
+
+    describe('list', () => {
+
+        it('renders without crashing', () => {
+            const div = document.createElement('div');
+            ReactDOM.render(<EmojiList emojis={testEmojis}/>, div);
+        });
+    
+        it('renders a snapshot', () => {
+            const tree = renderer.create(<EmojiList emojis={testEmojis}/>).toJSON();
+            expect(tree).toMatchSnapshot();
+        });
+
     });
 
-    it('renders the THUMBNAIL view  without crashing', () => {
-        const testEmojis = [];
-        const div = document.createElement('div');
-        ReactDOM.render(<EmojiThumbnail emojis={testEmojis}/>, div);
+    describe('thumbnail', () => {
+        
+        it('renders without crashing', () => {
+            const div = document.createElement('div');
+            ReactDOM.render(<EmojiThumbnail emojis={testEmojis}/>, div);
+        });
+
+        it('renders a snapshot', () => {
+            const tree = renderer.create(<EmojiThumbnail emojis={testEmojis}/>).toJSON();
+            expect(tree).toMatchSnapshot();
+        });
+
     });
 
-    it('renders the GALLERY view  without crashing', () => {
-        const testEmojis = [];
-        const testEmojiIndex = 1;
-        const div = document.createElement('div');
-        ReactDOM.render(<EmojiGallery emojis={testEmojis} emojiIndex={testEmojiIndex} />, div);
+    describe('gallery', () => {
+
+        it('renders without crashing', () => {
+            const testEmojiIndex = 1;
+            const div = document.createElement('div');
+            ReactDOM.render(<EmojiGallery emojis={testEmojis} emojiIndex={testEmojiIndex} />, div);
+        });
+
+        it('renders a snapshot', () => {
+            const tree = renderer.create(<EmojiGallery emojis={testEmojis}/>).toJSON();
+            expect(tree).toMatchSnapshot();
+        });
+
     });
 
 });
