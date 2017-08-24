727b57d7ca442b092947a0fd601122def108245c
Wendy Beck <webeck@gmail.com>
2017-08-23 21:46:38 -0700

    named all images and added titles and descriptions

--- src/data/hondas.js
+++ src/data/hondas.js
@@ -1,7 +1,103 @@
 const hondas = [{
-  title: 'Cute Bunny',
-  description: 'Isn\'t it fuzzy-wuzzy cutest thing you\'ve ever seen?',
-  url: './images/01-honda.jpg',
+  title: 'CB450 1986',
+  description: 'My first motorcycle. I still regret selling it.',
+  url: './images/01-honda-CB450-1986.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'CB1100F',
+  description: 'Photo taken in San Francisco.',
+  url: './images/02-honda-CB1100F.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'Avocado green Honda',
+  description: 'Photo taken in San Francisco.',
+  url: './images/03-honda.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'CB350',
+  description: 'Photo possibly taken in Portland.',
+  url: './images/04-honda-CB350.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'CBX550F',
+  description: 'Photo possibly taken in Portland.',
+  url: './images/05-honda-CBX550F.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'CB750F',
+  description: 'Photo taken on Valencia Street in San Francsico.',
+  url: './images/06-honda-CB750F.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'CX500',
+  description: 'This is my cousin Lucas\' bike. I keep asking him if he\'ll sell it to me but he hasn\'t so far. :(',
+  url: './images/07-honda-CX500.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'Honda',
+  description: 'Orange Honda of unknown engine size and my finger.',
+  url: './images/08-honda.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'CB750 2000',
+  description: 'This is my current bike. I was out riding around and turned to sharply too slowly and dropped it in someone\'s driveway. On a side street on a weekday afternoon in the suburbs. I waited for about 45 minutes for someone to come along and help me pick it up. On the plus side I got to learn how to replace a clutch lever after this broke mine off.',
+  url: './images/09-honda-CB750-2000.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'CB450',
+  description: 'Photo taken in Portland.',
+  url: './images/10-honda-CB450.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'CB450',
+  description: 'I found the same bike a second time.',
+  url: './images/11-honda-CB450.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'Hawk',
+  description: 'Photo taken in Portland.',
+  url: './images/12-honda-hawk.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'CB750',
+  description: 'An earlier model of my bike.',
+  url: './images/13-honda-CB750.jpg',
+  alt: 'honda!'
+},
+{
+  title: '1978 CB750',
+  description: 'This is the 1978 model of the bike I have now. It\'s in near-perfect shape.',
+  url: './images/14-honda-CB750-1978.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'CB350',
+  description: 'Lovely orange bike.',
+  url: './images/15-honda-CB350.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'Honda',
+  description: 'Not sure where I took this.',
+  url: './images/16-honda.jpg',
+  alt: 'honda!'
+},
+{
+  title: 'A different avocado green Honda',
+  description: 'Photo probably taken in San Francisco.',
+  url: './images/17-honda.jpg',
   alt: 'honda!'
 }]
 
