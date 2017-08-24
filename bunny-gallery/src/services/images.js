663ecdcf9bc3f5d3f48dbe341652b0f7256551e7
Meryl Turner <merylturner08@gmail.com>
2017-08-24 11:47:11 -0700

    proptypes added

--- bunny-gallery/src/services/images.js
+++ bunny-gallery/src/services/images.js
@@ -1,5 +1,3 @@
-import PropTypes from 'prop-types';
-
 let id = 0;
 const images = [
     { id: ++id, title: 'cute bunny', description: 'very cute bunny', url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg' },
@@ -7,7 +5,4 @@ const images = [
     { id: ++id, title: 'fairly cute bunny', description: 'pretty much a cute bunny', url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-110__605.jpg' },
 ];
 
-images.PropTypes = {
-    optionalArrayOf: PropTypes.arrayOf(PropTypes.object)
-}
 export default images;
