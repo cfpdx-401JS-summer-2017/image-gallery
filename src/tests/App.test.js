1717474de0f7ed59b9ece91525389e380ce8570d
Wendy Beck <webeck@gmail.com>
2017-08-24 14:56:50 -0700

    added snapshot tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-
+import { shallow, mount, render } from 'enzyme';
 import App from '../App';
 import renderer from 'react-test-renderer';
-
+import toJSON from 'enzyme-to-json';
+import Gallery from '../components/Gallery';
+import List from '../components/List';
+import Thumb from '../components/Thumb';
+import hondas from '../data/hondas-for-testing'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -12,39 +15,37 @@ it('renders without crashing', () => {
 });
 
 it('renders a snapshot in gallery view', () => {
-  const tree = renderer.create(<App/>).toJSON();
-  expect(tree).toMatchSnapshot();
+  const wrapper = mount(<Gallery hondas={hondas} />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
 });
 
 it('renders a snapshot in list view', () => {
-  const tree = renderer.create(<App/>).toJSON();
-  expect(tree).toMatchSnapshot();
+  const wrapper = shallow(<List hondas={hondas} />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
 });
 
 it('renders a snapshot in thumb view', () => {
-  const tree = renderer.create(<App/>).toJSON();
-  expect(tree).toMatchSnapshot();
+  const wrapper = render(<Thumb hondas={hondas} />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
 });
 
 it('deletes an image', () => {
   const wrapper = shallow(<App />);
   // expect(wrapper.state().playerRoom.key === 'westHall')
   // expect(wrapper.state().princRoom.key === 'office')
-})
+});
 
 it('adds an image', () => {
   const wrapper = shallow(<App />);
   // expect(wrapper.state().playerRoom.key === 'westHall')
   // expect(wrapper.state().princRoom.key === 'office')
-})
+});
 
 it('loads default images', () => {
   const wrapper = shallow(<App />);
   // expect(wrapper.state().playerRoom.key === 'westHall')
   // expect(wrapper.state().princRoom.key === 'office')
-})
-
-
+});
 
 // You also need to snapshot test:
 // * your imaging viewing components
