0720e4bdd39bc1d1a0c89d9b31d16095c1034e54
Christopher Thomson <christopher.thomson42@gmail.com>
2017-08-24 17:11:34 -0700

    Created test environment and rebuilt eslintrc

---
+++ src/tests/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import BunnyApp from '../BunnyApp';
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+  
+    const div = document.createElement('div');
+    ReactDOM.render(<BunnyApp />, div);
+});
+
+it('renders a snapshot', () => {
+    const tree = renderer.create(<BunnyApp/>).toJSON();
+    expect(tree).toMatchSnapshot();
+})
