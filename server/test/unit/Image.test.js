00fb171507948f740090558a3d4d271503c81c44
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-08-28 10:38:09 -0700

    Image model first validation test passes

--- server/test/unit/Image.test.js
+++ server/test/unit/Image.test.js
@@ -7,6 +7,11 @@ const expectedValidation = () => { throw new Error('expected validation errors')
 describe('images model', () => {
     
     it('validates a good model', () => {
-        
+        const image = new Image({ 
+            title: 'Test Bunny Title', 
+            description: 'Test Bunny Description', 
+            url: 'testbunny@test.com' 
+        });
+        return image.validate();
     });
 });
