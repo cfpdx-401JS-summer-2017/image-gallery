1b473207b4b1a1b36cdb3f01f8437589b67f049f
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-08-28 16:50:55 -0700

    moving files into separate repos for app and server

--- server/test/e2e/_db.js
+++
@@ -1,9 +0,0 @@
-process.env.MONGODB_URI = 'mongodb://localhost:27017/cutebunnies-test';
-require('../../lib/connect');
-const connection = require('mongoose').connection;
-
-module.exports = {
-    drop() {
-        return connection.dropDatabase();
-    }
-};
